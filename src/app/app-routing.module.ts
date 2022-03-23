67f9a7e3cd1631f1b25aa9bd4df03cbba2cc4a14
Gabriela Rojas <rojasgabriela901@gmail.com>
2022-03-22 22:01:01 -0700

    adding report page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,6 +19,10 @@ const routes: Routes = [
     path: 'profile',
     loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
   },
+  {
+    path: 'report',
+    loadChildren: () => import('./report/report.module').then( m => m.ReportPageModule)
+  },
 ];
 
 @NgModule({
---
+++ src/app/report/report.page.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit  } from '@angular/core';
+
+@Component({
+  selector: 'app-report',
+  templateUrl: './report.page.html',
+  styleUrls: ['./report.page.scss'],
+})
+export class ReportPage implements OnInit {
+  constructor() { }
+  
+  ngOnInit() {
+  }
+}
